1edac235e86152f3b7024f3b6bbe6d8c313ed57d
ギャル <gyaru@users.noreply.github.com>
2019-11-22 01:47:45 +0100

    huh

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,17 @@
+const htmlmin = require("html-minifier");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if(outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyURLs: true
+      });
+      return minified;
+    }
+    return content;
+  });
 };
